e414ce57788c640702a8f471f9570c50e8ca2e05
Tom Phillips <tom.phillips@perficient.com>
2020-03-02 16:31:25 -0500

    downloaded spring boot project and set dependencies

---
+++ src/main/java/com/prft/lab3server/Lab3ServerApplication.java
@@ -0,0 +1,13 @@
+package com.prft.lab3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prft/lab3server/Lab3ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prft.lab3server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
